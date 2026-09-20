a2ca62b4dbebc55e0f6e60198fc0692d0d0a1cf9
rukaiya2026 <rukaiyaraonaf@gmail.com>
2026-09-20 06:54:47 +0000

    Lesson1 homework done.

--- lesson1/homework/problems.js
+++ lesson1/homework/problems.js
@@ -1,25 +1,37 @@
 // Problem 1
 // Create a variable for your lucky number and print it.
 
-
+x = 7
+console.log("My favorite number is" +" "+ x )
 
 // Problem 2
 // Create a variable for the number of hours you sleep and print it.
 
-
+y = 9 
+console.log ("I sleep" + "  " + y + "hours a day" )
 
 // Problem 3
 // Create a variable for your favorite fruit and print it in a full sentence.
-
-
+z = "apple"
+console.log ("I like to eat" +" "+ z )
 
 // Problem 4
 // Create two variables: one for your city and one for your country.
 // Print them on 2 separate lines.
 
+ci= "Seattle"
+c= "USA"
+
+console.log("My city name is" + " " + ci)
+console.log("My country name is" + " " + c)
 
 
 // Problem 5
 // Create 3 variables: your pet's name, its age, and its type.
 // Print them on the same line in a full sentence.
 
+p= "muzze"
+a= 7
+t="male"
+
+console.log ("My pet name is " + " " + p + ", it is a " + t + " "+ "and it is "+ a + " "+ "years old")
